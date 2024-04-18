3b6d9177e71969d392782a3acf5f9ac1c1c78365
Sabasheikh44 <162295662+Sabasheikh44@users.noreply.github.com>
2024-04-19 01:56:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let c_value = {
+    PKR: 278.40,
+    UAE: 3.67,
+    USD: 1
+};
+let ans = await inquirer.prompt([{
+        type: "list",
+        name: "from",
+        message: "Converting From",
+        choices: ['PKR', 'USD', 'UAE']
+    },
+    {
+        type: "list",
+        name: "to",
+        message: "Converting to",
+        choices: ['PKR', 'USD', 'UAE']
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "your amount to convert"
+    }
+]);
+console.log(c_value[ans.to] / c_value[ans.from] * ans.amount);
